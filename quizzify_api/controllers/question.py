e7648f020fd2c22009aabb9d2b035628b78f39cf
andregom <andre.oliveira@ccc.ufcg.edu.br>
2022-08-20 23:42:55 -0300

    exposes model execution to API

--- quizzify_api/controllers/question.py
+++ quizzify_api/controllers/question.py
@@ -1,10 +1,12 @@
 import flask.scaffold
 flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
-from flask import request
+from flask import request, jsonify
 from flask_restx import Resource
 from http import HTTPStatus
 
 from quizzify_api.server.instance import server
+
+from ..quizzify_core.question_generation.question_generator import generate_question
 
 app, api = server.app, server.api
 
@@ -13,13 +15,22 @@ app, api = server.app, server.api
 class QuestionsController(Resource):
 
     def get(self, type, id):
-        try:
-            return self.get_question(type, id)
-        except Exception as error:
-            print(error)
-            return HTTPStatus.INTERNAL_SERVER_ERROR
+        # try:
+            return self.get_question(type, id, query = request.get_json())
+        # except Exception as error:
+        #     print(error)
+        #     return HTTPStatus.INTERNAL_SERVER_ERROR
 
     
     @staticmethod
-    def get_question(type, id):
-        return 'How are you doing?'
+    def get_question(type, id, query):
+        context = query.get('context', '')
+        answer = query.get('answer', '')
+        question = generate_question(context, answer)
+        return jsonify(question)
+
+
+
+
+
+        
